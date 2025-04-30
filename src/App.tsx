55ba52fbebc4593062a352688f4142ce762c44de
Rafael <guio@2be.com.br>
2025-04-30 19:45:00 -0300

    Fix: Corrigir salto inicial e flash de carregamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React /* , { Suspense, lazy } */ from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -6,11 +6,15 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import ContactDialog from "./components/ContactDialog";
 import { useContactDialog } from "./hooks/use-contact-dialog";
+// Importar diretamente os componentes Toaster
+import { Toaster } from "@/components/ui/toaster";
+import { ToasterWrapper } from "@/components/ui/sonner";
 
 const queryClient = new QueryClient();
 
-const LazyToaster = lazy(() => import('@/components/ui/toaster').then(module => ({ default: module.Toaster })));
-const LazyToasterWrapper = lazy(() => import('@/components/ui/sonner').then(module => ({ default: module.ToasterWrapper })));
+// Remover as importações lazy
+// const LazyToaster = lazy(() => import('@/components/ui/toaster').then(module => ({ default: module.Toaster })));
+// const LazyToasterWrapper = lazy(() => import('@/components/ui/sonner').then(module => ({ default: module.ToasterWrapper })));
 
 const App = () => {
   const { isOpen, setOpen, buttonText } = useContactDialog();
@@ -18,10 +22,11 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Suspense fallback={<div>Carregando...</div>}>
-          <LazyToaster />
-          <LazyToasterWrapper />
-        </Suspense>
+        {/* Remover o Suspense que envolvia os Toasters */}
+        {/* <Suspense fallback={<div>Carregando...</div>}> */}
+          <Toaster />
+          <ToasterWrapper />
+        {/* </Suspense> */}
         
         <ContactDialog 
           open={isOpen} 
